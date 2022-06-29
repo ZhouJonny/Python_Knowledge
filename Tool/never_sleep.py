5e36716035acb732359eea81f18e0e5e1ff72131
Jonny <904637097@qq.com>
2022-06-29 15:57:42 +0800

    Add never_sleep.py

---
+++ Tool/never_sleep.py
@@ -0,0 +1,15 @@
+"""
+Move the mouse every five seconds, so that the computer will never sleep
+"""
+import logging
+import random
+import time
+
+import pyautogui
+
+while True:
+    x = random.randint(-200, 200)
+    y = random.randint(-200, 200)
+    pyautogui.moveRel(x, y)
+    print(f'current location---x:{x} y:{y}')
+    time.sleep(5)
